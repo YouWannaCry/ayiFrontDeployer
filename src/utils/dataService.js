82ef91d78a76475678ebfc5dc83e6f0f909e7ede
YouWannaCry <arodrigoegea@gmail.com>
2022-10-19 21:39:22 -0300

    .

--- src/utils/dataService.js
+++ src/utils/dataService.js
@@ -1,9 +1,9 @@
 let persona = {
-    name: "Rodrigo",
-    cellphone: "Egea",
+    name: "Rodrigo Egea",
+    cellphone: "5492634400151",
     longitude: 0.0,
     latitude: 0.0,
-    contacts: [],
+    contacts: [ "" ],
 };
 
 const BASE_URL = "https://ayifrontdeployer.pages.dev";
@@ -11,31 +11,28 @@ const BASE_URL = "https://ayifrontdeployer.pages.dev";
 export const getUserLocation = async () => {
   navigator.geolocation.getCurrentPosition(
     ({ coords }) => {
-      persona.longitude = coords.longitude;
-      persona.latitude = coords.latitude;
-
-      console.log(
-        "Your location is aprox: " + persona.latitude + " " + persona.longitude
-      );
-      console.log(
+    persona.longitude = coords.longitude;
+    persona.latitude = coords.latitude;
+    console.log(
+        "Your location is aprox: " + persona.latitude + " " + persona.longitude + test
+    );
+    console.log(
         "https://www.google.com/maps/search/?api=1&query=" + persona.latitude + "%2c" + persona.longitude
-      )
-
-      fetch(BASE_URL + "/takeData", {
+    )
+    fetch(BASE_URL + "/takeData", {
         method: "POST",
         body: JSON.stringify(persona),
         headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials" : true ,
+        "Content-Type": "application/json",
         },
-      })
+    })
         .then((res) => res.json())
         .then((res) => console.log(res.json))
         .then((data) => console.log("Data sent " + data));
     },
     (error) => {
-      alert("No se pudo obtener la geolocalización.");
-      console.log(error);
+    alert("No se pudo obtener la geolocalización.");
+    console.log(error);
     }
-  );
+    );
 };  
